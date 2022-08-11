336294e4620c3fe3cd72181e0fc189b2b7e2c2aa
Saadat Ali <saadatali0202@gmail.com>
2022-08-11 21:49:17 +0500

    Add fontawesome to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
 import '@/styles/global.css'
+import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
+import { config } from "@fortawesome/fontawesome-svg-core";
 import type { AppProps } from 'next/app'
+
+config.autoAddCss = false;
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
