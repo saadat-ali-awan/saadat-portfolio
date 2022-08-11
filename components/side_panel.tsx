e63114e8746ea3c2a1bce29fb7390acb8efb6c92
Saadat Ali <saadatali0202@gmail.com>
2022-08-11 21:49:53 +0500

    Write side panel component

---
+++ components/side_panel.tsx
@@ -0,0 +1,127 @@
+import Image from "next/image";
+// Import the FontAwesomeIcon component
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// Import the brands library to get access to the brands
+import { faGithub, faLinkedin, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { useEffect } from "react";
+import { useState } from "react";
+import styles from './side_panel.module.css';
+
+export default function SidePanel() {
+  const [url, setUrl] = useState("");
+  useEffect(
+    () => {
+      // get resume link from api
+      const fetchUrl = async () => {
+        const response = await fetch("/api/resume-link");
+        const data = await response.json();
+        setUrl(data.url);
+      };
+      fetchUrl();
+    },[])
+  return (
+    <div className={styles["side-panel"]}>
+      <div className={styles["side-panel__header"]}>
+        <div className={styles["side-panel__profile-image"]}>
+          <Image 
+            src="https://avatars.githubusercontent.com/u/35307862?v=4"
+            alt="profile_image"
+            width={200}
+            height={200}
+            className={styles["side-panel_profile-image"]}
+            data-testid="profile_image"
+          />
+        </div>
+        <div className={styles["side-panel__header-title"]}>
+          <h2 role='heading'>Saadat Ali</h2>
+        </div>
+        <div className={styles["side-panel__header-subtitle"]}>
+          <h3 data-testid="profession">Full Stack Developer</h3>
+        </div>
+        <div className={styles["side-panel__header-social"]}>
+          <a href="https://www.github.com/Saadat123456" title="GitHub Link" target="_blank" rel="noopener">
+            <FontAwesomeIcon
+              icon={faGithub}
+              data-testid="social-media-icon"
+              size="2x"
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/saadatali1999/" title="LinkedIn Link" target="_blank" rel="noopener">
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              data-testid="social-media-icon"
+              size="2x"
+            />
+          </a>
+          <a href="https://twitter.com/Saadat02021999" title="Twitter Link" target="_blank" rel="noopener">
+            <FontAwesomeIcon
+              icon={faTwitter}
+              data-testid="social-media-icon"
+              size="2x"
+            />
+          </a>
+        </div>
+      </div>
+      <div className={styles["side-panel__body"]}>
+        <div className={styles["side-panel__body-item"]}>
+          <div className={styles["side-panel__body-item-title"]}>
+            <h3>Email</h3>
+          </div>
+          <div className={styles["side-panel__body-item-content"]}>
+            <p>
+              <a href="mailto:saadatali0202@gmail.com" data-testid="email">saadatali0202@gmail.com</a>
+            </p>
+          </div>
+        </div>
+        <div className={styles["side-panel__body-item"]}>
+          <div className={styles["side-panel__body-item-title"]}>
+            <h3>Phone</h3>
+          </div>
+          <div className={styles["side-panel__body-item-content"]}>
+            <p>
+              <a href="tel:+923365910676" data-testid="phone">+923365910676</a>
+            </p>
+          </div>
+        </div>
+        <div className={styles["side-panel__body-item"]}>
+          <div className={styles["side-panel__body-item-title"]}>
+            <h3>Age</h3>
+          </div>
+          <div className={styles["side-panel__body-item-content"]}>
+            <p>
+              <span data-testid="age">22</span>
+            </p>
+          </div>
+        </div>
+        <div className={styles["side-panel__body-item"]}>
+          <div className={styles["side-panel__body-item-title"]}>
+            <h3>Address</h3>
+          </div>
+          <div className={styles["side-panel__body-item-content"]}>
+            <p>
+              <span data-testid="location">Rawalpindi, Pakistan</span>
+            </p>
+          </div>
+        </div>
+        <div className={styles["side-panel__body-item"]}>
+          <div className={styles["side-panel__body-item-title"]}>
+            <h3>Language</h3>
+          </div>
+          <div className={styles["side-panel__body-item-content"]}>
+            <p>
+              <span data-testid="language">English</span>
+              ,&nbsp;
+              <span data-testid="language">Urdu</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="side-panel__footer">
+        <div className="side-panel__footer-item">
+          <a href={url} title="Resume" target="_blank" rel="noopener" download>Download Resume</a>
+        </div>
+      </div>
+    </div>
+  );
+
+}
