56ca74ac9767bb16b5010114491e9bfff434c644
Saadat Ali <saadatali0202@gmail.com>
2022-08-11 21:47:23 +0500

    Fix test for home page

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,12 +1,20 @@
 import { render, screen } from '@testing-library/react'
 import Home from '@/pages/index'
+import fetch from 'jest-fetch-mock';
 
 describe('Home', () => {
+  afterEach(() => {
+    fetch.resetMocks();
+  }),
+  
+  beforeEach(async () => {
+    fetch.mockResponseOnce(JSON.stringify({ rates: { CAD: 1.42 } }));
+    render(<Home />);
+  }),
+  
   it('renders a heading', () => {
-    render(<Home />)
-
     const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
+      name: /I'm Saadat Ali/i,
     })
 
     expect(heading).toBeInTheDocument()
