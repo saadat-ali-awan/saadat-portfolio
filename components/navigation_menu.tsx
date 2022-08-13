2baf2c30f4b7b1bb142b6817ca9bfdad12edbaef
Saadat Ali <saadatali0202@gmail.com>
2022-08-13 05:58:07 +0500

    Add navigation menu at right side

---
+++ components/navigation_menu.tsx
@@ -0,0 +1,24 @@
+import { useSectionState } from 'context/section_state_provider';
+import { useRef, useState } from 'react';
+import { useEffect } from 'react';
+import styles from './navigation_menu.module.css'
+
+export default function NavigationMenu() {
+  const { sectionState } = useSectionState();
+
+  return (
+    <div className={styles["navigation-menu"]}>
+      <ul className="menu">
+        {
+          Array.from(Array(sectionState.total_sections).keys()).map(i => {
+            return (
+              <li key={i} className="menu-item" >
+                <a href={`#section-${i + 1}`} title='section-link' className={sectionState.section_number === i ? 'active' : ''} />
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
+  );
+}
