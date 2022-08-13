aface9aed2a1ffb043265090ee5962ddde5128c5
Saadat Ali <saadatali0202@gmail.com>
2022-08-13 05:59:37 +0500

    Wrap app with provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import '@/styles/global.css'
 import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
 import { config } from "@fortawesome/fontawesome-svg-core";
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { SectionStateProvider } from 'context/section_state_provider';
 
 config.autoAddCss = false;
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SectionStateProvider >
+      <Component {...pageProps} />
+    </SectionStateProvider>
+  );
 }
