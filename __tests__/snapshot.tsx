f368f9f375695c45ab97fa8b01181a5b901e3f7c
Saadat Ali <saadatali0202@gmail.com>
2022-08-15 20:42:35 +0500

    Fix snapshot test

--- __tests__/snapshot.tsx
+++ __tests__/snapshot.tsx
@@ -1,13 +1,20 @@
 import { render } from '@testing-library/react'
 import Home from '@/pages/index'
 import fetch from 'jest-fetch-mock';
+import { SectionStateProvider } from '../context/section_state_provider';
+
 afterEach(() => {
   fetch.resetMocks();
 }),
 beforeEach(async () => {
   fetch.mockResponseOnce(JSON.stringify({ url: '' }));
 }),
+
 it('renders homepage unchanged', () => {
-  const { container } = render(<Home />)
-  // expect(container).toMatchSnapshot()
+  const { container } = render(
+    <SectionStateProvider>
+      <Home />
+    </SectionStateProvider>
+  )
+  expect(container).toMatchSnapshot()
 })
