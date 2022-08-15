c52c76294e36a78d96a5ad6af85437898ab6e3f8
Saadat Ali <saadatali0202@gmail.com>
2022-08-15 20:47:43 +0500

    Add first section

--- components/homepage/first_section.tsx
+++ components/homepage/first_section.tsx
@@ -1,13 +1,31 @@
-import { faReact } from '@fortawesome/free-brands-svg-icons';
 import styles from './first_section.module.css';
+
 export default function FirstSection() {
   return (
     <section className={`profile-section ${styles["first-section"]}`} id="section-1">
       <div className={styles['left-side']}>
-        <div>
-          <i className="devicon-devicon-plain colored"></i>
+        <div className={styles['icons-container']}>
+          <div className={styles['icon-container']}>
+            <img alt='Ruby on Rails' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-plain-wordmark.svg" />
+          </div>
+          <div className={styles['icon-container']}>
+            <img alt='RSpec' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rspec/rspec-original-wordmark.svg" />
+          </div>
+          <div className={styles['icon-container']}>
+            <img alt='Postgres SQL' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" />
+          </div>
         </div>
-        <div>Second</div>
+        <div className={styles['icons-container']}>
+          <div className={styles['icon-container']}>
+            <img alt='React' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
+          </div>
+          <div className={styles['icon-container']}>
+            <img alt='Jest' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg" />
+          </div>
+          <div className={styles['icon-container']}>
+            <img alt='Redux' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" />
+          </div>
+        </div>
       </div>
       <img
         src="/full_pic.png"
