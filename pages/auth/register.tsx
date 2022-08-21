1bc53a4bfe0b228d9a19b3e12c95292487a6e4dd
Saadat Ali <saadatali0202@gmail.com>
2022-08-22 04:47:19 +0500

    Add register page

---
+++ pages/auth/register.tsx
@@ -0,0 +1,83 @@
+import React, { ChangeEvent, useState } from "react";
+import { FormEvent } from "react";
+
+export default function Register() {
+  // Create Registration Form
+  const [form, setForm] = useState({
+    username: "",
+    password: "",
+    passwordConfirm: "",
+  });
+
+  // handle form input change
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.name);
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  // handle form submit
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    // post data to /api/auth/register
+    fetch("/api/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form),
+    }).then((res) => {
+      if (res.status === 200) {
+        console.log("Registration Successful");
+      } else {
+        console.log("Registration Failed");
+      }
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          className="form-control"
+          id="username"
+          name="username"
+          placeholder="Username"
+          value={form.username}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+          name="password"
+          placeholder="Password"
+          value={form.password}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="passwordConfirm">Password Confirm</label>
+        <input
+          type="password"
+          className="form-control"
+          id="passwordConfirm"
+          name="passwordConfirm"
+          placeholder="Password Confirm"
+          value={form.passwordConfirm}
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+}
