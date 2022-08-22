c014c344a4542c48a0f227e908478a608debfd25
Saadat Ali <saadatali0202@gmail.com>
2022-08-23 04:27:07 +0500

    Complete user registration process

--- pages/api/auth/register.tsx
+++ pages/api/auth/register.tsx
@@ -1,18 +1,47 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
+import PasswordValidator from "password-validator";
 
 const prisma = new PrismaClient();
+const schema = new PasswordValidator();
+
+schema.is().min(8)
+  .is().max(100)
+  .has().uppercase()
+  .has().lowercase()
+  .has().digits(2)
+  .has().not().spaces();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const data = req.body;
+  const passwordIssues = schema.validate(data.password, { details: true }) as {validation: string, arguments: number, message: string}[];
+  if (passwordIssues.length > 0) {
+    res.status(500).json({
+      message: passwordIssues.map(issue => issue.message).join(", ")
+    });
+    return
+  }
+
   // Add post data to database in BackendUser table
   try {
-    await prisma.backendUser.create({
-      data: {
-        name: req.body.username,
-        password: req.body.password,
+    const response = await fetch(`http://localhost:3000/api/github-data`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
       },
+      body: JSON.stringify({
+        name: data.username,
+        password: data.password,
+      }),
     });
-    res.status(200).json({ message: "Registration Successful" });
+
+    const responseJson = await response.json();
+
+    if (response.status === 200) {
+      res.status(200).json({ message: "Registration Successful" });
+    } else {
+      res.status(500).json(responseJson);
+    }
   } catch (err: any) {
     res.status(500).json({ message: err.message });
   }
