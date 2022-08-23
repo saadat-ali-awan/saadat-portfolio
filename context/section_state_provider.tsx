63cd9b6ac70fc6e668b26a7ff31f76bb80e7fd00
Saadat Ali <saadatali0202@gmail.com>
2022-08-24 01:07:36 +0500

    Create function to set state of section provider

--- context/section_state_provider.tsx
+++ context/section_state_provider.tsx
@@ -8,43 +8,51 @@ interface SectionState {
 const useSectionStateController = () => {
   const [sectionState, setSectionState] = useState<SectionState>( { section_number: 0, total_sections: 0 } );
   const stateRef = useRef(sectionState);
+  let main: HTMLElement | null = null;
+  let sections: HTMLCollectionOf<HTMLElement>;
+
   useEffect(() => {
-    // get main
-    const main = document.querySelector('main');
-    // get all sections
-    const sections = document.getElementsByClassName('profile-section') as HTMLCollectionOf<HTMLElement>;
-    const handleScroll = () => {
-      if (main) {
-        // get current scroll position
-        const scroll_position = main.scrollTop;
-        // check if a section is visible
-        for (let i = 0; i < sections.length; i++) {
-          const section = sections[i];
-          const half_height = window.innerHeight / 2;
-          const section_top = section.offsetTop - half_height;
-          const section_bottom = (section_top + section.offsetHeight) - half_height;
-          if (scroll_position >= section_top && (scroll_position - half_height) <= section_bottom) {
-            stateRef.current = { ...stateRef, section_number: i, total_sections: sections.length };
-            setSectionState({ section_number: i, total_sections: sections.length });
-            break;
-          }
+    return () => main?.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const handleScroll = () => {
+    if (main) {
+      // get current scroll position
+      const scroll_position = main.scrollTop;
+      // check if a section is visible
+      for (let i = 0; i < sections.length; i++) {
+        const section = sections[i];
+        const half_height = window.innerHeight / 2;
+        const section_top = section.offsetTop - half_height;
+        const section_bottom = (section_top + section.offsetHeight) - half_height;
+        if (scroll_position >= section_top && (scroll_position - half_height) <= section_bottom) {
+          stateRef.current = { ...stateRef, section_number: i, total_sections: sections.length };
+          setSectionState({ section_number: i, total_sections: sections.length });
+          break;
         }
       }
     }
+  }
+
+  const initSectionState = () => {
+    // get main
+    main = document.querySelector('main');
+    // get all sections
+    sections = document.getElementsByClassName('profile-section') as HTMLCollectionOf<HTMLElement>;
 
     setSectionState({ ...sectionState, total_sections: sections.length, section_number: 0 });
+
     main?.addEventListener('scroll', handleScroll);
-    return () => main?.removeEventListener('scroll', handleScroll);
-  }, []);
-  return { sectionState, setSectionState };
+  }
+  return { sectionState, initSectionState };
 }
 
-const SectionStateContext = createContext<ReturnType<typeof useSectionStateController>>({ sectionState: { section_number: 0, total_sections: 0 }, setSectionState: () => {} });
+const SectionStateContext = createContext<ReturnType<typeof useSectionStateController>>({ sectionState: { section_number: 0, total_sections: 0 }, initSectionState: () => {} });
 
 export const SectionStateProvider = ({children} : {children: ReactNode}) => {
-  const { sectionState, setSectionState } = useSectionStateController();
+  const { sectionState, initSectionState } = useSectionStateController();
   return (
-    <SectionStateContext.Provider value={{sectionState, setSectionState}}>
+    <SectionStateContext.Provider value={{sectionState, initSectionState}}>
       {children}
     </SectionStateContext.Provider>
   );
