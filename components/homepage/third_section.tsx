0eba765d99d1d8391d00486b9f865d0264c93ecc
Saadat Ali <saadatali0202@gmail.com>
2022-08-25 01:28:06 +0500

    Remove extra sections

--- components/homepage/third_section.tsx
+++ components/homepage/third_section.tsx
@@ -108,39 +108,3 @@ export function ThirdSection() {
     </section>
   );
 }
-
-export function FourthSection() {
-  return (
-    <section className="profile-section" id="section-4">
-      <div className="container">
-        <h1 className="title">
-          I'm <strong>Saadat Ali</strong> Fourth Container
-        </h1>
-      </div>
-    </section>
-  );
-}
-
-export function FifthSection() {
-  return (
-    <section className="profile-section" id="section-5">
-      <div className="container">
-        <h1 className="title">
-          I'm <strong>Saadat Ali</strong> Fifth Container
-        </h1>
-      </div>
-    </section>
-  );
-}
-
-export function SixthSection() {
-  return (
-    <section className="profile-section" id="section-6">
-      <div className="container">
-        <h1 className="title">
-          I'm <strong>Saadat Ali</strong> Sixth Container
-        </h1>
-      </div>
-    </section>
-  );
-}
