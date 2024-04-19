fc9963a9ae3f2ab291ba40e83c72b708a4214ddf
Saadat Ali <saadatali0202@gmail.com>
2024-04-19 11:08:17 +0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '@/pages/index.module.css'
+import Script from 'next/script'
 
 export default function Home() {
   return (
@@ -12,6 +13,14 @@ export default function Home() {
       <div className={styles['main-body']}>
         Hello World
       </div>
+      
+            <!-- AudienceLab Pixel Code -->
+            <Script>
+                const pixelId = '167c6fff-4e90-4a7f-b24a-c6afc1669f29';
+            </Script>
+            <Script src="https://cdn.audiencelab.io/pixel_V2.js"></Script>
+            <!-- End AudienceLab Pixel Code -->
+        
     </div>
   )
 }
